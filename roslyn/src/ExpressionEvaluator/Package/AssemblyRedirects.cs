2c4cf669515616266e2baa7886ee1a31620e4549
Daniel <dpenalba@codicesoftware.com>
2017-03-20 14:47:12 +0000

    First change

--- roslyn/src/ExpressionEvaluator/Package/AssemblyRedirects.cs
+++ roslyn/src/ExpressionEvaluator/Package/AssemblyRedirects.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Roslyn.VisualStudio.Setup;
-
+// fisrt change
 [assembly: ProvideRoslynBindingRedirection("Microsoft.CodeAnalysis.CSharp.ExpressionEvaluator.ExpressionCompiler.dll")]
 [assembly: ProvideRoslynBindingRedirection("Microsoft.CodeAnalysis.CSharp.ExpressionEvaluator.ResultProvider.dll")]
 [assembly: ProvideRoslynBindingRedirection("Microsoft.CodeAnalysis.ExpressionEvaluator.ExpressionCompiler.dll")]
